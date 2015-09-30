ea63140759d396b5210592b1a3da66840a58a134
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-30 12:14:25 -0700

    listening on correct port

--- WoodbrickOven.java
+++ WoodbrickOven.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class WoodbrickOven {
     public static void main(String[] args) {
+        port(8082);
         get("/cook", (req, res) -> {
           Thread.sleep(3);
           return "done";
